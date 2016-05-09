54e4fd673c0d27b1f5f9d3a59e9170df12c0e697
Ian Watson <ianwatson@redhat.com>
2016-05-09 12:03:52 +0100

    Started refactoring to Java DSL

---
+++ routing/src/main/java/com/redhat/iw/routes/CSV2JSONRouteBuilder.java
@@ -0,0 +1,37 @@
+package com.redhat.iw.routes;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.Uri;
+
+public class CSV2JSONRouteBuilder extends RouteBuilder {
+
+	//@EndpointInject	(uri = "dozer:transformCSV?unmarshalId=csv&marshalId=myjson&mappingFile=transformations/csv2json.xml&targetModel=org.globex.Account")
+	@EndpointInject (ref = "transform")
+	Endpoint transform;
+	
+	@EndpointInject (uri = "{{fileInput}}")
+	Endpoint input;
+	
+	@EndpointInject (uri = "{{fileOutput}}")
+	Endpoint output;
+	
+	@EndpointInject (uri = "{{fileError}}")
+	Endpoint error;
+	
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		onException(java.lang.IllegalArgumentException.class)
+			.handled(true)
+			.to(error);
+		
+		from(input).routeId("CSV2JSON")
+			.split(body(String.class).tokenize("\n"))
+				.to(transform)
+				.to(output);
+		
+	}
+
+}
